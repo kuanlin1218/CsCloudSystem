e0b0f7e881d0867bb83a8850aeaa7c6394829a65
kuanlin1218 <64317567+kuanlin1218@users.noreply.github.com>
2025-04-15 15:52:32 +0800

    Add files via upload

--- 詮栩CNC雲端系統_ver_1/config.js
+++ 詮栩CNC雲端系統_ver_1/config.js
@@ -1,5 +1,11 @@
 // config.js
+
+//GAS部署網址
 const scriptUrl = 'https://script.google.com/macros/s/AKfycbyDiAaYD4ipjBqRuomgNWdk_AU7g6Ef0nvTzLfAPhC4RQYJuxYbsxZAUYk6KO17KN8_/exec';  // 在這裡替換為您的 GAS URL
 
 
+//navbar 公司logo
+const LOGO_URL = "https://kuanlin1218.github.io/image/logo_S.png";
 
+// 網址favicon.ico
+const FAVICON_URL = "https://kuanlin1218.github.io/image/favicon.ico";
