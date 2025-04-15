6fa1710dad8f7298fda29c6c38b34620816961ae
kuanlin1218 <64317567+kuanlin1218@users.noreply.github.com>
2025-04-15 13:09:00 +0800

    Add files via upload

--- 詮栩CNC雲端系統_ver_1/config.js
+++ 詮栩CNC雲端系統_ver_1/config.js
@@ -1,5 +1,5 @@
 // config.js
-const scriptUrl = 'https://script.google.com/macros/s/AKfycbwzb7KOjACvxe7r39zQKsoj9kXO6Uj0D66SOfVV27U23ntgMJ0UWDK9pnOts0UYE-vc/exec';  // 在這裡替換為您的 GAS URL
+const scriptUrl = 'https://script.google.com/macros/s/AKfycbyDiAaYD4ipjBqRuomgNWdk_AU7g6Ef0nvTzLfAPhC4RQYJuxYbsxZAUYk6KO17KN8_/exec';  // 在這裡替換為您的 GAS URL
 
 
 
